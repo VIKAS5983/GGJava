3c9bbf0f23b7c89ac8fbb499e0695c76c41d18be
Vikas Reddy Varakantham <vvarakan@opentext.com>
2018-11-15 06:35:31 +0530

    Tree Traversal

--- Tree/src/gg/binaryTree/TreeNodeTraversel.java
+++ Tree/src/gg/binaryTree/TreeNodeTraversel.java
@@ -1,6 +1,9 @@
 package gg.binaryTree;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
+import java.util.Vector;
 
 public class TreeNodeTraversel {
 	
@@ -23,7 +26,19 @@ public class TreeNodeTraversel {
 		postOrderTraversol(head);
 		
 		System.out.println("Inorder Traversal WithoutRecrusion ");
+		
 		inOrderTraversalIterator(head);
+        TreeNode root = new TreeNode(8); 
+        root.left = new TreeNode(3); 
+        root.right = new TreeNode(10); 
+        root.left.left = new TreeNode(1); 
+        root.left.right = new TreeNode(6); 
+        root.right.right = new TreeNode(14); 
+        root.right.right.left = new TreeNode(13); 
+        root.left.right.left = new TreeNode(4); 
+        root.left.right.right = new TreeNode(7); 
+
+		diagonalTraversal(root);
 		return;
 	}
 
@@ -96,4 +111,34 @@ public class TreeNodeTraversel {
 		
 	}
 
+	public static void diagonalTraversal(TreeNode head){
+		Map<Integer,Vector<TreeNode>> mp=new HashMap<Integer, Vector<TreeNode>>();
+		int diagonalOrder=1;
+		diagonalTraversalUtil(head, mp,diagonalOrder);
+		System.out.println("Diagonal Order as follows ");
+		 for(Map.Entry<Integer, Vector<TreeNode>> en:mp.entrySet()){
+			 System.out.print(en.getKey()+" : -> ");
+			 
+			 for(int i=0; i<en.getValue().size(); i++){
+				 System.out.print(en.getValue().get(i).data+" ");
+			 }
+			 System.out.println();
+		 }
+	}
+	
+	public static void diagonalTraversalUtil(TreeNode head,Map<Integer,Vector<TreeNode>> mp,int dO){
+		if(head==null){
+			return;
+		}
+		if(mp.get(dO)==null){
+			Vector<TreeNode> v=new Vector<TreeNode>();
+			v.add(head);
+			 mp.put(dO, v);
+		}else
+		mp.get(dO).add(head);
+		
+		diagonalTraversalUtil(head.left, mp, dO+1);
+		diagonalTraversalUtil(head.right,mp,dO);
+		
+	}
 }
